b5aab83a674b39719eddd6f5f78f7083dead4955
yoex <ytentertainmnet94@gmail.com>
2023-04-05 15:04:09 -0400

    checkpoint para trabajar en casa del front end

--- pages/raffle.js
+++ pages/raffle.js
@@ -4,121 +4,82 @@ import styles from "../styles/Home.module.css"
 import { Form, useNotification, Button } from "web3uikit"
 import { useMoralis, useWeb3Contract } from "react-moralis"
 import { ethers } from "ethers"
-import nftAbi from "../constants/BasicNft.json"
-import nftMarketplaceAbi from "../constants/NftMarketplace.json"
-import networkMapping from "../constants/networkMapping.json"
+import raffleAbi from "../constants/RafflePersonalizado.json"
+import networkMappingRaffle from "../constants/networkMappingRaflle.json"
+
 import { useEffect, useState } from "react"
 
 export default function Raffle() {
     const { chainId, account, isWeb3Enabled } = useMoralis()
     const chainString = chainId ? parseInt(chainId).toString() : "31337"
-    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
+    const raffleAddress = chainId ? networkMappingRaffle[chainString].RafflePersonalizado[0] : null
     const dispatch = useNotification()
-    const [proceeds, setProceeds] = useState("0")
+    const [rondaActual, setRondaActual] = useState("0")
+    const entranceFee = ethers.utils.parseEther("0.0032369315488695")
 
     const { runContractFunction } = useWeb3Contract()
-
-    async function approveAndList(data) {
-        console.log("Approving...")
-        const nftAddress = data.data[0].inputResult
-        const tokenId = data.data[1].inputResult
-        const price = ethers.utils.parseUnits(data.data[2].inputResult, "ether").toString()
-
-        const approveOptions = {
-            abi: nftAbi,
-            contractAddress: nftAddress,
-            functionName: "approve",
-            params: {
-                to: marketplaceAddress,
-                tokenId: tokenId,
-            },
-        }
-
-        await runContractFunction({
-            params: approveOptions,
-            onSuccess: (tx) => handleApproveSuccess(tx, nftAddress, tokenId, price),
-            onError: (error) => {
-                console.log(error)
-            },
-        })
-    }
-
-    async function handleApproveSuccess(tx, nftAddress, tokenId, price) {
-        console.log("Ok! Now time to list")
-        await tx.wait()
-        const listOptions = {
-            abi: nftMarketplaceAbi,
-            contractAddress: marketplaceAddress,
-            functionName: "listItem",
-            params: {
-                nftAddress: nftAddress,
-                tokenId: tokenId,
-                price: price,
-            },
-        }
-
-        await runContractFunction({
-            params: listOptions,
-            onSuccess: () => handleListSuccess(),
-            onError: (error) => console.log(error),
-        })
-    }
-
-    async function handleListSuccess() {
-        dispatch({
-            type: "success",
-            message: "NFT listing",
-            title: "NFT listed",
-            position: "topR",
-        })
-    }
 
     const handleWithdrawSuccess = () => {
         dispatch({
             type: "success",
-            message: "Withdrawing proceeds",
+            message: "Se compro el billete de loteria",
             position: "topR",
         })
     }
 
     async function setupUI() {
-        const returnedProceeds = await runContractFunction({
+        const returnRondaActual = await runContractFunction({
             params: {
-                abi: nftMarketplaceAbi,
-                contractAddress: marketplaceAddress,
-                functionName: "getProceeds",
-                params: {
-                    seller: account,
-                },
+                abi: raffleAbi,
+                contractAddress: raffleAddress,
+                functionName: "obtenerActualRondaLoteria",
             },
             onError: (error) => console.log(error),
         })
-        if (returnedProceeds) {
-            setProceeds(returnedProceeds.toString())
+        if (returnRondaActual) {
+            setRondaActual(returnRondaActual.toString())
         }
+    }
+
+    async function comprandoBillete(data) {
+        const numeroTicket = data.data[0].inputResult
+        console.log("Comprando... numero jugado: " + numeroTicket)
+
+        await runContractFunction({
+            params: {
+                abi: raffleAbi,
+                contractAddress: raffleAddress,
+                functionName: "enterRaffle",
+                params: {
+                    numTicket: numeroTicket,
+                },
+                msgValue: entranceFee,
+            },
+            onError: (error) => console.log(error),
+            onSuccess: () => handleWithdrawSuccess,
+        })
     }
 
     useEffect(() => {
         setupUI()
-    }, [proceeds, account, isWeb3Enabled, chainId])
+    }, [rondaActual, account, isWeb3Enabled, chainId])
 
     return (
         <div className={styles.container}>
-            <Button
-                onClick={() => {
-                    runContractFunction({
-                        params: {
-                            abi: nftMarketplaceAbi,
-                            contractAddress: marketplaceAddress,
-                            functionName: "withdrawProceeds",
-                            params: {},
-                        },
-                        onError: (error) => console.log(error),
-                        onSuccess: () => handleWithdrawSuccess,
-                    })
-                }}
-                text="Comprar billete"
-                type="button"
+            <h1>Estamos en la ronda # ${rondaActual}</h1>
+            <Form
+                onSubmit={comprandoBillete}
+                data={[
+                    {
+                        name: "numero de ticket",
+                        type: "number",
+                        inputWidth: "20%",
+                        value: "",
+                        key: "numTicket",
+                    },
+                ]}
+                title="Loteria basica, digita un numero y compra un billete"
+                id="Main Form"
             />
         </div>
     )
